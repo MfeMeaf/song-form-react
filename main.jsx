10f50a4021e55d2005b2cc2c1d49eb72ace54525
MfeMeaf <80445981+MfeMeaf@users.noreply.github.com>
2023-01-27 10:55:46 +0100

    Minor Changes
    While trying to fix buttons not changing the embed content

--- main.jsx
+++ main.jsx
@@ -4,33 +4,33 @@ var embeddiv = document.getElementById("embed-iframe");
 var embedlist = document.getElementById("embed-list");
 var root = document.getElementById("form");
 
-let bearer = "BQBAnKPJzfSm_Roa3zkIdUUOxgzVnkpKgeHNht_kSv77_To33p7Et5GNIM2j878Zn94pZ2jrf7E_tK3StQxQ7cHXGnPviU5XitRU2FIKpn8R4kFecOEQc3m4sn_xMgYnPUFxiS0uSK8tbwvomLKmH2v4J8n6PVxmAJkC-MnGo8RmI7_oFLUU4QoOHUBP4hdZ4cQC"
-
-let songs = [];
-
 window.onSpotifyIframeApiReady = (IFrameAPI) => {
     let element = document.getElementById('embed-iframe');
     let options = {
         width: '60%',
         height: '200',
-        uri: "spotify:track:30SCXVFyQGOFMdKnbgJS18"
+        uri: "spotify:track:0WhSlHpA9T6S8pKyz7DRBx"
 };
     let callback = (EmbedController) => {
         document.querySelectorAll('ul > li > button').forEach(
-        episode => {
-            episode.addEventListener('click', () => {
-          EmbedController.loadUri(episode.dataset.spotifyId)
+        song => {
+            song.addEventListener('click', () => {
+          EmbedController.loadUri(song.dataset.spotifyId)
         });
       })
   };
     IFrameAPI.createController(element, options, callback);
 };
 
+let bearer = "BQAt7WPX1R66YyRA0-w4Nidl4pFCh3hCKpA5zMmPEN8r9oWit_W1Txi4g8HvK55K191lQzTc8B8qGxtHhnOSIFJK5kxQ_LWZRzeqqSU_pPDFLirKRkO4ek7xyZBdbLXRhggpaxstyK6vna5IxIDYvbpl1d-0SkJ_DtkQORd5OPhA1uhAA2R7nwHQ0mNyZGkqg0sh"
+
+let songs = [];
+
 var SongLista = React.createClass({
     render(){
         return (
             <li>
-                <button data-spotify-id="spotify:episode:7makk4oTQel546B0PZlDM5">
+                <button  data-spotify-id={this.props.song.uri}>
                         {this.props.song.name}
                 </button>
             </li>
@@ -38,13 +38,14 @@ var SongLista = React.createClass({
     }
 });
 
+
 var Form = React.createClass({
     handleSubmit(event){
     event.preventDefault();
     fetch(`https://api.spotify.com/v1/search?q=${event.target.songN.value}&type=track&limit=10&offset=5`, {
         method: "GET",
         headers: {
-            Authorization: `Bearer BQBAnKPJzfSm_Roa3zkIdUUOxgzVnkpKgeHNht_kSv77_To33p7Et5GNIM2j878Zn94pZ2jrf7E_tK3StQxQ7cHXGnPviU5XitRU2FIKpn8R4kFecOEQc3m4sn_xMgYnPUFxiS0uSK8tbwvomLKmH2v4J8n6PVxmAJkC-MnGo8RmI7_oFLUU4QoOHUBP4hdZ4cQC` 
+            Authorization: `Bearer ${bearer}` 
         }
     })
     .then(response => response.json())
